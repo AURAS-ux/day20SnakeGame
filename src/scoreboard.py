d519fc6847ae427f7310441798dd3291b67de31f
Auras Paltanea <92265800+AURAS-ux@users.noreply.github.com>
2022-02-22 22:00:12 +0200

    added highscore sistem

--- src/scoreboard.py
+++ src/scoreboard.py
@@ -7,7 +7,8 @@ class Scoreboard(Turtle):
         super().__init__()
         self.penup()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as file:
+            self.hsc = int(file.read())
         self.hideturtle()
         self.goto(0, 270)
         self.pendown()
@@ -16,11 +17,14 @@ class Scoreboard(Turtle):
 
     def writeScore(self):
         self.clear()
-        self.write(arg=f"Score:{self.score} High Score:{self.highscore}", move=False, align="center", font=("Times New Roman", 20, "normal"))
+        self.write(arg=f"Score:{self.score} High Score:{self.hsc}", move=False, align="center",
+                   font=("Times New Roman", 20, "normal"))
 
     def reset(self):
-        if self.score > self.highscore:
-            self.highscore = self.score
+        if self.score > self.hsc:
+            self.hsc = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.hsc}")
         self.score = 0
         self.writeScore()
 
